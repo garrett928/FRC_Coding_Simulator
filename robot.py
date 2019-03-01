9044eedf0c8bbaa516ba8763f9fc24662c1e2bce
Garrett Hart <garrett7621@gmail.com>
2019-03-01 01:38:52 -0500

    adds a background image to the robot instead of a black rectangle

--- robot.py
+++ robot.py
@@ -8,10 +8,25 @@ import driver_station as ds
 x = field.RED_2[0]
 y = field.RED_2[1]
 
+# robot width and height
+width = 60
+height = 60
+
+# create robot image
+robot_img = pygame.image.load("robot.png")
+# robot robot left to face "forward"
+robot_img = pygame.transform.rotate(robot_img, -90)
+# create a robot surface to be added to the main window
+robot = pygame.Surface([width, height])
+# add the robot image to the robot
+robot.blit(robot_img, [0, 0])
+
 
 def draw():
     """Draw robot to screen"""
-    pygame.draw.rect(win.screen, (0, 0, 0), [x - 20, y - 20, 40, 40], 0)
+    # Add the robot surface to the main window surface.
+    # The x and y from the field module is given from the center
+    win.screen.blit(robot, [x - width/2, y - height/2])
 
 
 def drive():
@@ -34,4 +49,4 @@ def drive():
         y -= 1
     if ds.RIGHT_PRESSED:
         print("Right arrow pressed")
-        y += 1
+        y+= 1
