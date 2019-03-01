88e5d813ed91d2d564e7fbf30f519708644ef92d
Garrett Hart <garrett7621@gmail.com>
2019-03-01 00:34:07 -0500

    adds function to check and report arrow key presses

--- driver_station.py
+++ driver_station.py
@@ -1,9 +1,31 @@
 import pygame
 
-"""have a class with members like up arrow, down arrow, etc
-    with wrapper methods"""
+
+# setup key delay though pygame
+
+
+
+UP_PRESSED = False
+DOWN_PRESSED = False
+LEFT_PRESSED = False
+RIGHT_PRESSED = False
 
 
 def check_keyboard():
     """Check and update the keyboard state"""
-    pass
+
+    # use global vars to update
+    global UP_PRESSED
+    global DOWN_PRESSED
+    global LEFT_PRESSED
+    global RIGHT_PRESSED
+
+    # get all pressed keys on the keyboard
+    key = pygame.key.get_pressed()
+
+    # update global vars
+    UP_PRESSED = key[pygame.K_UP]
+    DOWN_PRESSED = key[pygame.K_DOWN]
+    LEFT_PRESSED = key[pygame.K_LEFT]
+    RIGHT_PRESSED = key[pygame.K_RIGHT]
+
