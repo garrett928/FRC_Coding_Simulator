ecaeb19adcef2ca4422dddccc578d99dd5200176
Garrett Hart <garrett7621@gmail.com>
2019-03-01 03:09:23 -0500

    adds math to drive with arrow keys. driving works and is semi smooth

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 import field
 import pygame
+import math
 import window as win
 import driver_station as ds
 
@@ -8,45 +9,75 @@ import driver_station as ds
 x = field.RED_2[0]
 y = field.RED_2[1]
 
+# field heading, keep track of robot angle
+theta = 0
+delta_theta = 0
 # robot width and height
 width = 60
 height = 60
 
 # create robot image
-robot_img = pygame.image.load("robot.png")
-# robot robot left to face "forward"
-robot_img = pygame.transform.rotate(robot_img, -90)
+org_robot_img = pygame.image.load("robot.png")
+
 # create a robot surface to be added to the main window
-robot = pygame.Surface([width, height])
+robot = pygame.Surface([width, height], pygame.SRCALPHA)
 # add the robot image to the robot
-robot.blit(robot_img, [0, 0])
+# robot robot left to face "forward"
+robot.blit(pygame.transform.rotate(org_robot_img, -90), [0, 0])
 
 
 def draw():
     """Draw robot to screen"""
+
     # Add the robot surface to the main window surface.
     # The x and y from the field module is given from the center
-    win.screen.blit(robot, [x - width/2, y - height/2])
+    rotated_robot_img = pygame.transform.rotate(org_robot_img, theta)
+    rotated_robot = pygame.transform.rotate(robot, theta)
+    #win.screen.blit(rotated_robot, [x + width/2, y + height/2])
+    win.screen.blit(rotated_robot, rotated_robot.get_rect(center=(x, y)))
 
 
 def drive():
 
     # use global x and y
-    global x, y
+    global x, y, delta_theta, theta
+
+    # reset theta
+    delta_theta = 0
+
+    # throttle
+    throttle = 0
 
     # update keyboard arrows
     ds.check_keyboard()
 
     # print of anything is pressed
     if ds.UP_PRESSED:
-        print("Up arrow pressed")
-        x += 1
+        # increase throttle when pressing forward
+        throttle += 4
     if ds.DOWN_PRESSED:
-        print("Down arrow pressed")
-        x -= 1
+        # decrease throttle when pressing forward
+        throttle -= 4
     if ds.LEFT_PRESSED:
-        print("Left arrow pressed")
-        y -= 1
+        # increase amount to turn by -2
+        delta_theta += 4
     if ds.RIGHT_PRESSED:
-        print("Right arrow pressed")
-        y+= 1
+        # increase amount to turn by 2
+        delta_theta -= 4
+
+    # turn robot
+    turn(delta_theta)
+
+    # calc new x and Y
+    x += throttle * math.cos(theta * math.pi/180)
+    y -= throttle * math.sin(theta * math.pi/180)
+    print("x, y = (" + str(x) +", " + str(y) + ")")
+
+
+def turn(angle):
+    global theta
+    print("angle: " + str(angle))
+    print("theta " + str(theta))
+
+    # increase field angle
+    theta += angle
