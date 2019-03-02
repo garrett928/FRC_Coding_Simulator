86f6e283c49c261ae426c7ce6e68d3b0dad65aa9
Garrett Hart <garrett7621@gmail.com>
2019-03-02 00:41:41 -0500

    adds small usage message

--- robot_code.py
+++ robot_code.py
@@ -1,6 +1,22 @@
 import robot
 
+"""
+    Robot Rules:
+        robot.drive: Uses the arrow keys to drive the robot
+        robot.forwards: Moves the robot forwards a given amount
+        robot.backwards: Moves the robot backwards a given amount
+        robot.turn_left: Turns the robot left a given amount
+        robot.turn_right: Turn the robot right a given amount
+        robot.stop: Makes the robot program stop running
+"""
+
 
 def robot_code():
-    robot.drive()
-
+    robot.forward(20)
+    robot.turn_right(90)
+    robot.forward(40)
+    robot.turn_left(90)
+    robot.forward(40)
+    robot.turn_right(90)
+    robot.backwards(40)
+    robot.stop()
